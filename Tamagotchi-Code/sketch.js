887636413aa2ec09733dfa3f3407149efa7b0d3f
Feni14 <58942843+Feni14@users.noreply.github.com>
2021-06-25 18:37:29 +0200

    Textboxen & Restartbutton

--- Tamagotchi-Code/sketch.js
+++ Tamagotchi-Code/sketch.js
@@ -5,14 +5,17 @@ let Background1 = loadImage('assets/heart-background.png');
 let startPic = loadImage('assets/start-button.png');
 let textBox = loadImage('assets/text-box.png');
 let Buttons = loadImage('assets/buttons.png');
+let sleepZ = loadImage('assets/sleep.png');
+let foodScreen = loadImage('assets/food-screen.png');
+let heart = loadImage('assets/heart.png');
 let Font = loadFont('assets/ARCADECLASSIC.TTF');
-let startButton = new Button(150, 300, 260, 70);
+let startButton = new Button(150, 350, 260, 70);
+let restartButton = new Button(150, 350, 260, 70);
 let care = new Care(foodScreen, sleepZ, heart, textBox);
 let loop = 0;
 let state = "start";
 
 //Food
-let foodScreen = loadImage('assets/food-screen.png');
 let foodButton = new Button(420, 10, 130, 50);
 let pizzaButton = new Button(100, 425, 80, 80);
 let cupcakeButton = new Button(220, 425, 80, 80);
@@ -21,20 +24,21 @@ let nom = loadImage('assets/nom.png');
 let feedingTime = 0;
 let eating = false;
 let full = false;
+let fullTextBox = 0;
 
 //Sleep
-let sleepZ = loadImage('assets/sleep.png');
 let sleepButton = new Button(420, 65, 130, 50);
 let napTime = 0;
 let nap = false;
 let hyped = false;
+let hypedTextBox = 0;
 
 //Pat
-let heart = loadImage('assets/heart.png');
 let patButton = new Button(420, 120, 130, 50);
 let socializing = 0;
 let patting = false;
 let annoyed = 0;
+let annoyedTextBox = 0;
 
 //Characters
 let CinnaPic = loadImage('assets/Cinnamoroll.png');
@@ -70,15 +74,18 @@ function mouseClicked() {
          name = "MyMelody";
          state = "game";
          }
+      } else if (restartButton.hitTest() && state == "failed"){
+         state = "start";
       }
    
    //Feeding
    if (care.hunger <= 90) {
    if (foodButton.hitTest()) {
       care.foodValue = true;
-      full = false;
       }
-   } else {full = true;}
+   } else if (care.hunger > 90 && foodButton.hitTest())
+   {full = true;}
+
    if (care.foodValue == true) {
    if (pizzaButton.hitTest()) {
       care.foodValue = false;
@@ -111,7 +118,7 @@ function mouseClicked() {
       hyped = false;
       care.level = care.level + 1;
       }
-   } else {hyped = true;}
+   } else if (care.energy > 90 && sleepButton.hitTest()) {hyped = true;}
 
    //Patting
    if (care.love <= 90) {
@@ -121,7 +128,7 @@ function mouseClicked() {
       annoyed = false;
       care.level = care.level + 1;
       }
-   } else {annoyed = true;}
+   } else if (care.love > 90 && patButton.hitTest()) {annoyed = true;}
 }
 
 
@@ -132,9 +139,9 @@ function draw() {
   
    //Start screen
    if (state == "start"){
-      image(startPic, 150, 300);
+      image(startPic, 150, 350);
       textSize(30);
-      text("START", 230, 345);
+      text("START", 230, 395);
    }
    else if (state == "characterChoice") {
 
@@ -171,7 +178,7 @@ function draw() {
 
 
    //Decrease of parameters
-   if (loop >= 100) {
+   if (loop >= 50) {
       loop = 0;
    }
    loop = loop + 1;
@@ -186,6 +193,10 @@ function draw() {
    if(care.love >= 5) {
    care.love = care.love - 5;
    }
+   }
+
+   if (care.hunger <= 0 || care.energy <= 0 || care.love <= 0) {
+      state = "failed";
    }
 
    //nom emote
@@ -232,17 +243,66 @@ function draw() {
    }
 
    //notification when full
-   if (full == true) {
+   if(full == true){
+      fullTextBox = fullTextBox + 1;
+   }
+   if(fullTextBox >= 50){
+      fullTextBox = 0;
+      full = false;
+   }
+
+   if (fullTextBox >= 1) {
       care.textBox();
       text(name + " is not hungry anymore!", 80, 450);
    }
 
+   //notification when hyped
+   if(hyped == true){
+      hypedTextBox = hypedTextBox + 1;
+   }
+   if(hypedTextBox >= 50){
+      hypedTextBox = 0;
+      hyped = false;
+   }
+
+   if (hyped == true) {
+      care.textBox();
+      text(name + " is too hyped to sleep!", 80, 450);
+   }
+
+   //notification when annoyed
+   if(annoyed == true){
+      annoyedTextBox = annoyedTextBox + 1;
+   }
+   if(annoyedTextBox >= 50){
+      annoyedTextBox = 0;
+      annoyed = false;
+   }
+
+   if (annoyed == true) {
+      care.textBox();
+      text(name + "'s social battery is empty! They need some space.", 80, 450);
+   }
+
    //level
-   if (level = 10){
+   if (care.level == 10){
       care.level();
    }
 
 }
+
+   else if (state == "failed") {
+      textSize(50);
+      fill(255, 0, 0);
+      text("FAILED", 195, 150);
+      fill(0, 0, 0);
+      textSize(25);
+      text("Oh no! " + name + " didn't feel well.", 110, 250);
+      text("You should take better care of them next time.", 30, 300);
+      image(startPic, 150, 350);
+      textSize(30);
+      text("RESTART", 210, 395);
+   }
 }
 
 window.draw = draw;
